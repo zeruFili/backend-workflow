29558766e727931f1e0839b2777ca76004f3f477
zeruFili <zerubabelfilmon1@gmail.com>
2026-06-16 19:33:07 +0300

    feat: display latest review details in application workflow
    * Add latest review information section between Submission & Review and Case Study
    * Show review message, review status, and review stage

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,11 +5,12 @@ import { userController } from "../controllers/user.controller";
 
 const router = Router();
 
+router.get("/", authenticate, authorize(UserRole.CEO, UserRole.GENERAL_MANAGER), (req, res, next) => userController.findAll(req, res, next));
+router.get("/:id", authenticate, authorize(UserRole.CEO, UserRole.GENERAL_MANAGER), (req, res, next) => userController.findById(req, res, next));
+
 router.use(authenticate, authorize(UserRole.CEO));
 
-router.get("/", (req, res, next) => userController.findAll(req, res, next));
 router.post("/", (req, res, next) => userController.create(req, res, next));
-router.get("/:id", (req, res, next) => userController.findById(req, res, next));
 router.patch("/:id/status", (req, res, next) => userController.updateStatus(req, res, next));
 router.patch("/:id", (req, res, next) => userController.update(req, res, next));
 router.delete("/:id", (req, res, next) => userController.softDelete(req, res, next));
